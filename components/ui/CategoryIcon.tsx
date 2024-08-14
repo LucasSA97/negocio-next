f386ead996229338f7cb30ebf402ae4e1523afc8
Lucas <lucassarachu7@gmail.com>
2024-08-14 23:39:51 +0200

    Update saving category selection

--- components/ui/CategoryIcon.tsx
+++ components/ui/CategoryIcon.tsx
@@ -1,15 +1,18 @@
+'use client'
 import { Category } from "@prisma/client"
 import Link from "next/link"
 import Image from "next/image"
+import { useParams } from "next/navigation"
 
 type CategoryIconPros = {
     category: Category
 }
 export default function CategoryIcon({
-category, params
+category
 }: CategoryIconPros) {
+    const params = useParams<{category: string}>()
   return (
-    <div className={`flex items-center gap-4 w-full border-t border-rose-200 p-3 last-of-type:border-b`}>
+    <div className={` ${category.slug === params.category ? 'bg-red-400  text-white border-indigo-200' : ''} flex items-center gap-4 w-full border-t border-rose-200 p-3 last-of-type:border-b`}>
         <div className="w-16 h-16 relative">
         <Image 
         fill
