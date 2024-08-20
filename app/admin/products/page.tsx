f712365c9d4d985970c8029a9d805b921e31f5ba
Lucas <lucassarachu7@gmail.com>
2024-08-20 22:36:04 +0200

    Navegation in to the admin dashboard

--- app/admin/products/page.tsx
+++ app/admin/products/page.tsx
@@ -1,12 +1,11 @@
+import Header from "@/components/ui/Header"
 import ToastNotification from "@/components/ui/ToastNotification"
 
 export default function ProductsPage() {
-  console.log('desde el servidor')
   return (
-    <div>
-    <div>Products Page</div>
-    <ToastNotification/>
-    </div>
+    <>
+    <Header>Administrar Productos</Header>
+    </>
 
   )
 }
