2928b448ae7605e4dc0f8b21d80276eaf9c680a8
Lucas <lucassarachu7@gmail.com>
2024-08-22 16:15:30 +0200

    Check orders completes and validation in db

--- actions/complete-order-action.ts
+++ actions/complete-order-action.ts
@@ -1,6 +1,21 @@
 'use server'
 
-export async function completeOrder(){
+import { prisma } from "@/src/lib/prisma"
 
+export async function completeOrder(formData : FormData) {
+    const orderId = formData.get('order_id')!
+    try {
+        await prisma.order.update({
+            where: {
+                id: +orderId
+            },
+            data: {
+                status: true,
+                orderReadyAt: new Date(Date.now())
+            }
+        })
+    } catch (error) {
+        console.log(error)
+    }
     
 }
