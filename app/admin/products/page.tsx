09219de01f139ae957f168e75a00cf7672761e22
Lucas <lucassarachu7@gmail.com>
2024-08-24 16:20:53 +0200

    Include the category in the table

--- app/admin/products/page.tsx
+++ app/admin/products/page.tsx
@@ -4,17 +4,28 @@ import ToastNotification from "@/components/ui/ToastNotification"
 import { prisma } from "@/src/lib/prisma"
 
 async function getProducts(){
-  const products = await prisma.product.findMany()
+  const products = await prisma.product.findMany({
+    include: {
+      category: true
+    }
+  })
 
   return products
 }
 
+export type ProductsWithCategory = Awaited<ReturnType<typeof getProducts>>
+
 export default async function ProductsPage() {
+
   const products = await getProducts()
+  
   return (
     <>
-    <Header>Administrar Productos</Header>
-    <ProductTable products={products} />
+      <Header>Administrar Productos</Header>
+
+      <ProductTable 
+          products={products} 
+          />
     </>
 
   )
--- components/products/ProductsTable.tsx
+++ components/products/ProductsTable.tsx
@@ -1,10 +1,18 @@
-import { Product } from '@prisma/client';
+import { Category, Product } from '@prisma/client';
 import { products } from '../../prisma/data/products';
 import { formatPrice } from '@/src/utils';
 import Link from 'next/link';
+import { ProductsWithCategory } from '@/app/admin/products/page';
+
 type ProductTableProps = {
-    products: Product[]
+     products: ProductsWithCategory
 }
+
+// type ProductTableProps = {
+//      products: ({
+//         category: Category
+//     } & Product)[]
+// }
 
 export default function ProductTable({products}: ProductTableProps) {
     return (
@@ -39,7 +47,7 @@ export default function ProductTable({products}: ProductTableProps) {
                                    { formatPrice(product.price)}
                                    </td> 
                                    <td className="whitespace-nowrap px-3 py-4 text-sm text-gray-500">
-                                    {product.categoryId}
+                                    {product.category.name}
                                    </td>
                                    <td className="relative whitespace-nowrap py-4 pl-3 pr-4 text-right text-sm font-medium sm:pr-0">
                                     <Link 
