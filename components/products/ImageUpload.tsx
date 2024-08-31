3d41190127be0777bf8136360a213879b59305d5
Lucas <lucassarachu7@gmail.com>
2024-08-31 20:19:00 +0200

    Update ImageUpload component

--- components/products/ImageUpload.tsx
+++ components/products/ImageUpload.tsx
@@ -1,7 +1,33 @@
-import React from 'react'
-
+'use client'
+import { CldUploadWidget } from "next-cloudinary"
+import { TbPhotoPlus } from 'react-icons/tb'
 export default function ImageUpload() {
+
   return (
-    <div>ImageUpload</div>
+    <CldUploadWidget 
+        onSuccess={(result , {widget} ) => {
+            console.log(result , widget)
+        }}
+        options={{
+            maxFiles: 1,
+        }}
+        uploadPreset='gu7gvb2a'>
+      
+        {({open}) => (
+            <>
+                <div className="space-y-2">
+
+                    <label className="text-slate-800">Imagen Producto</label>
+                    <div 
+                        onClick={() => open()}
+                        className="relative cursor-pointer hover:opacity-70 transition p-10 border-neutral-300 flex flex-col justify-center items-center gap-4 text-neutral-600 bg-slate-100 border-2 rounded-xl">
+                        <TbPhotoPlus size={50} />
+                        <p className="text-lg font-semibold">Agregar Imagen</p>
+                    </div>
+                </div>
+            </>
+        )}
+
+    </CldUploadWidget>
   )
 }
