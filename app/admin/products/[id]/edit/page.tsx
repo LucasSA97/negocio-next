693d5f469fb0db72cc93a720ce6a00869ec6ce0d
Lucas <lucassarachu7@gmail.com>
2024-09-04 11:59:07 +0200

    Update edit  product form

--- app/admin/products/[id]/edit/page.tsx
+++ app/admin/products/[id]/edit/page.tsx
@@ -1,3 +1,6 @@
+import EditProductForm from "@/components/products/EditProductForm"
+import ProductForm from "@/components/products/ProductForm"
+import Header from "@/components/ui/Header"
 import { prisma } from "@/src/lib/prisma"
 import { notFound } from "next/navigation"
 
@@ -19,6 +22,12 @@ export default async function EditProductsPage({params} : {params: {id: string}}
     const product = await getProductById(+params.id)
 
   return (
-    <div>page</div>
+    <>
+    <Header>Editar Producto: {product.name}</Header>
+    <EditProductForm>
+      <ProductForm 
+      product={product}/>
+    </EditProductForm>
+    </>
   )
 }
