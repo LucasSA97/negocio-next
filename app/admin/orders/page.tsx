f47c0ef3997de418d91bff637451f3e1bbaff33c
Lucas <lucassarachu7@gmail.com>
2024-09-05 09:54:27 +0200

    Create end point api

--- app/admin/orders/page.tsx
+++ app/admin/orders/page.tsx
@@ -1,28 +1,33 @@
 import OrderCard from '@/components/order/OrderCard'
 import Header from '@/components/ui/Header'
 import { prisma } from '@/src/lib/prisma'
+import { revalidatePath } from 'next/cache'
 
 async function getPendingOrders() {
-  const orders = await prisma.order.findMany({
-    where: {
-      status: false,
-    },
-    include: {
-      orderProducts: {
-        include: {
-          product: true, 
-        }
-      }
-    }
-  })
-  return orders
+  
 }
 export default async function OrdersPage() {
   const orders = await getPendingOrders()
 
+  const refreshOrders = async () => {
+    'use server'
+
+    revalidatePath('/admin/orders')
+  }
+
   return (
     <>
       <Header>Administrar Ordenes</Header>
+
+      <form action={refreshOrders}>
+
+      <input type="submit"
+        className="bg-amber-400 w-full lg:w-auto text-xl px-10 py-3 text-center cursor-pointer"
+        value='Actualizar Ordenes' />
+
+      </form>
+
+
       {orders.length ? (
         <div className='grid grid-cols-1 lg:grid-cols-2 2xl:grid-cols-3 gap-4 mt-5 items-start'>
           {orders.map(order => (  
